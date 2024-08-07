1146e782f0c9c6457fbdd7d55bcd4a8d66a11dc7
Abduraim Abduraimov <abduraimabduraimov@Abduraims-MacBook-Pro.local>
2024-08-08 01:40:00 +0500

    end swiper mistakes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const body = document.querySelector("body")
+const threeDots = document.querySelector(".header__dots")
 const navBurger = document.querySelector(".nav__burger")
 const navBurgerLine = document.querySelector(".nav__burger-line")
 const navMenu = document.querySelector(".nav__menu")
@@ -9,7 +10,10 @@ const newsSwiper = new Swiper(".news__swiper", {
     },
         spaceBetween: 30,
         breakpoints: {
-            850: {
+            1024: {
+                slidesPerView: "1.6"
+            },
+            1603: {
                 slidesPerView: "2.6"
             },
         },
@@ -42,3 +46,44 @@ navBurger.addEventListener("click", function () {
     navBurgerLine.classList.toggle("active")
     navMenu.classList.toggle("active")
 })
+
+document.addEventListener("mousemove", function(event) {
+    
+    const speed = threeDots.getAttribute("data-speed")
+    
+    const X = event.clientX / 10 * speed
+    const Y = event.clientY / 10 * speed
+
+    threeDots.style.transform = `translate(${X}px, ${Y}px)`
+})
+
+const span = document.querySelectorAll(".rating__card-num span")
+let stopNumbers
+
+function timer(elem, number, index = 0) {  
+    index++
+    elem.innerHTML = index
+
+    stopNumbers = setTimeout(() => {
+        timer(elem, number, index)
+    }, 30);
+
+    if (index == number) {
+        clearTimeout(stopNumbers)
+    }
+}
+
+const rating = document.querySelector(".rating")
+let stopTimer = true
+
+function intro(entry) {
+    if (entry[0].isIntersecting && stopTimer) {
+        stopTimer = false
+        for (let i = 0; i < span.length; i++) {
+            timer(span[i], span[i].innerHTML)
+        }
+    }
+}
+
+const observer = new IntersectionObserver(intro, { threshold: 0.7 })
+observer.observe(rating)

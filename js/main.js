c21b5544a95868b71e2d82134932a464108149db
Abduraimov Abduraim <abdura1moffbro08@gmail.com>
2024-08-08 18:23:29 +0500

    some js works

--- js/main.js
+++ js/main.js
@@ -19,3 +19,43 @@ document.addEventListener("mousemove", function(event) {
 
     threeDots.style.transform = `translate(${X}px, ${Y}px)`
 })
+
+const introImage = document.querySelector(".intro__image")
+const SectionIntro = document.querySelector(".intro")
+
+function introSection(enter) {
+    if (enter[0].isIntersecting) {
+        introImage.classList.add("active")
+    }
+}
+
+const introObserver = new IntersectionObserver(introSection, { threshold: 1 })
+introObserver.observe(SectionIntro)
+
+
+const reasonsCard = document.querySelectorAll(".reasons__card")
+const reasons = document.querySelector(".reasons")
+const reasonsCards = document.querySelector(".reasons__cards")
+
+function reasonsSection(beginnig) {
+    if (beginnig[0].isIntersecting) {
+        reasonsCards.classList.add("appear")
+    }
+}
+
+const reasonsObserver = new IntersectionObserver(reasonsSection, { threshold: 0.5 })
+reasonsObserver.observe(reasons)
+
+
+const plansCard = document.querySelectorAll(".plans__card")
+const plansCards = document.querySelector(".plans__cards")
+const plans = document.querySelector(".plans")
+
+function plansSection(begin) {
+    if (begin[0].isIntersecting) {
+        plansCards.classList.add("card")
+    }
+}
+
+const plansObserver = new IntersectionObserver(plansSection, { threshold: 0.5 })
+plansObserver.observe(plans)
